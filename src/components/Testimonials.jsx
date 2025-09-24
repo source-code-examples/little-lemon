4eb37d466c881f42c7b45762216b2db1d7b0f575
WuKe <118019715+source-code-examples@users.noreply.github.com>
2025-09-24 20:47:35 +0200

    Update Testimonials component with responsive design and styling

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Testimonials.css";
+
+import faceOne from "../assets/images/image-face-1.jpg";
+import faceTwo from "../assets/images/image-face-2.jpg";
+import faceThree from "../assets/images/image-face-3.jpg";
+import faceFour from "../assets/images/image-face-4.jpg";
+
+const Testimonials = () => {
+  const reviews = [
+    {
+      rating: 4.5,
+      image: faceOne,
+      name: "Pete",
+      text: "Simply delicious! The best paella in town.",
+    },
+    {
+      rating: 5,
+      image: faceTwo,
+      name: "Xiao",
+      text: "Great atmosphere and very friendly staff.",
+    },
+    {
+      rating: 4,
+      image: faceThree,
+      name: "Lisa",
+      text: "I love the vegetarian options!",
+    },
+    {
+      rating: 5,
+      image: faceFour,
+      name: "David",
+      text: "Fantastic service and the desserts were out of this world!",
+    },
+  ];
+
+  return (
+    <section className="testimonials">
+      <h2>Testimonials</h2>
+
+      <div className="grid">
+        {reviews.map((review, index) => (
+          <div className="card" key={index}>
+            <div className="stars">
+              {[...Array(5)].map((_, i) => {
+                const fullStars = Math.floor(review.rating);
+                const hasHalfStar = review.rating % 1 !== 0;
+
+                if (i < fullStars) {
+                  return <i key={i} className="fa-solid fa-star"></i>;
+                } else if (i === fullStars && hasHalfStar) {
+                  return (
+                    <i key={i} className="fa-solid fa-star-half-stroke"></i>
+                  );
+                } else {
+                  return <i key={i} className="fa-regular fa-star"></i>;
+                }
+              })}
+            </div>
+            <div className="info">
+              <img src={review.image} alt={`Pic of ${review.name}`} />
+              <aside>{review.name}</aside>
+            </div>
+            <p className="text">"{review.text}"</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero.jsx";
 import Hightlights from "../components/Highlights.jsx";
-import Testimonials from "../components/Testimonials";
+import Testimonials from "../components/Testimonials.jsx";
 import About from "../components/About";
 import "./Home.css";
 
